d95af84216292840ae4239b39ad542c04aa6f544
charles <charles_hinolan@dlsu.edu.ph>
2024-03-05 12:24:24 +0800

    initial commit

---
+++ hi.R
@@ -0,0 +1,28 @@
+library(syuzhet)
+dataset <- read.csv("netflix_cleaned.csv")
+
+
+# Remove commas from the "mood" column
+dataset$mood <- gsub(",", "", dataset$mood)
+
+dataset$mood <- trimws(dataset$mood)
+dataset$mood <- tolower(dataset$mood)
+
+mood_filtered <- unique(dataset$mood)
+print(mood_filtered)
+
+movie <- ("Drive (2019)\n")
+#cat("Movie: ", movie)
+sample_text <- "This movie gave me a profound feeling of sorrow. It was horribly executed and the writing was garbage."
+sample_word <- "Four animal friends get a taste of the wild life when they break out of captivity at the Central Park Zoo and wash ashore on the island of Madagascar."
+
+cat("Sample review: ",sample_text,"\n\n")
+sample_sentences <- get_sentences(sample_text)
+#print(sample_sentences)
+
+sentiment_data <- get_nrc_sentiment(sample_sentences)
+
+mood_sentiment <- get_nrc_sentiment(sample_word)
+print(sentiment_data[1:8])
+cat("Mood Sentiment:\n")
+print(mood_sentiment[1:8])
