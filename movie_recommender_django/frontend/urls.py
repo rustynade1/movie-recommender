46f06e669f92d6671472486a1da27bddd26b953c
drimag <rafaelvedimagiba@gmail.com>
2024-03-20 04:34:09 +0800

    added search-movies to urls

--- movie_recommender_django/frontend/urls.py
+++ movie_recommender_django/frontend/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('search-movies', index)
     # path('join', index),
     # path('create', index),
     # path('join/1', index)
