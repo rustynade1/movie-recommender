93ac4c3aeb7286567a8f2bee160dad3fac8b04f8
drimag <rafaelvedimagiba@gmail.com>
2024-03-20 10:30:16 +0800

    message for movie not found in db in search movies

--- movie_recommender_django/frontend/src/components/SearchMovies.js
+++ movie_recommender_django/frontend/src/components/SearchMovies.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Box, TextField, List, ListItem, ListItemText, Avatar } from '@mui/material';
+import { Box, TextField, List, ListItem, ListItemText, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default class SearchMovies extends Component {
@@ -46,9 +46,10 @@ export default class SearchMovies extends Component {
            
            sx={{ marginBottom: '16px', width: '400px' }}
          />
-         <List sx= {{display: 'flex', flexDirection: 'row'}}>
-           {filteredMovies.map(movie => (
-             <ListItem button key={movie.id} component={Link} to={`/write-review/${movie.id}`} 
+        <List sx= {{display: 'flex', flexDirection: 'row'}}>
+          {filteredMovies.length > 0 ? (
+            filteredMovies.map(movie => (
+              <ListItem button key={movie.id} component={Link} to={`/write-review/${movie.id}`} 
                 sx={{ 
                   alignItems: 'center', 
                   width: 250, 
@@ -57,34 +58,36 @@ export default class SearchMovies extends Component {
                   border: '1px solid #000', 
                   margin: '16px' 
                 }}>
-
                 <Box
-                  component="img"
-                  sx={{
+                component="img"
+                sx={{
                     height: 300,
                     width: '100%', 
                     maxHeight: 300, 
                     maxWidth: 350, 
                     objectFit: 'cover',
-                  }}
-                  alt={movie.title}
-                  src={movie.image || defaultImage}
+                }}
+                alt={movie.title}
+                src={movie.image || defaultImage}
                 />
-
                 <Box 
-                  sx={{
+                sx={{
                     border: '1px solid #000',
                     borderRadius: '4px',
                     margin: '16px',
                     width: "100%"
-                  }}>
-                  <ListItemText primary={movie.title} secondary={movie.genre}/>
+                }}>
+                <ListItemText primary={movie.title} secondary={movie.genre}/>
                 </Box>
-                
               </ListItem>
-           ))}
-         </List>
-       </Box>
-     );
+            ))
+            ) : (
+              <Box sx={{ textAlign: 'center', width: '100%', height: '100vh' }}>
+                <Typography variant='h2'>Sorry, we can't seem to find that...</Typography>
+              </Box>
+            )}
+        </List>
+      </Box>
+    );
   }
  }
